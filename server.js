54f91e631d01f599e94fbbc7e59ffe971f979edf
grant2223 <grant@findfitpeople.com>
2026-05-23 10:22:26 +0400

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware - CORS Headers
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  const allowedOrigins = ['https://ffppassport.com', 'https://www.ffppassport.com', 'http://localhost:3000'];
+  const origin = req.headers.origin;
+  
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
